92761b429f176ab3732a937e67904c6bdc3aa920
Ruslan Sevostyanov <zetoster@gmail.com>
2025-11-29 10:59:07 +0300

    Added cpp sucks example

---
+++ cpp_examples/cpp_sucks.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+double m[] = { 7709179928849219.0, 771 };
+
+int main() { 
+	//m[1]-- ? m[0]*=2,main() : printf((char*)m);
+	if (m[1]--) 
+	{
+		m[0] *= 2;
+		main();
+	} 
+	else
+		printf((char*)m);
+}
