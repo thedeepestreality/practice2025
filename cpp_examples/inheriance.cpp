77d575cdeb214f531d55126a476d7d986480fdf1
rsevostyanov <rsevostyanov@gtinform.ru>
2026-04-11 20:35:02 +0300

    added dynamic_cast example

--- cpp_examples/inheriance.cpp
+++ cpp_examples/inheriance.cpp
@@ -15,6 +15,12 @@ struct Cat : Animal {
     void roar() const override {
         std::cout << "Meow!\n";
     }
+
+    // void fit_in_box() {
+    //     std::cout << "Cat is in a box (again)\n";
+    // }
+
+    int spy_data;
 };
 
 struct Dog : Animal {
@@ -33,12 +39,23 @@ struct Platypus : Animal {
 int main(){
     std::cout << sizeof(Animal) << '\n';
     Cat c1;
+    c1.spy_data = 42;
     Dog g;
     Cat c2;
+    c2.spy_data = 43;
     Platypus p;
     Animal* animals[] = {&c1, &g, &c2, &p};
-    for (Animal* animal: animals)
+    for (Animal* animal: animals){
         animal->roar();
+        // Cat* c_ptr = reinterpret_cast<Cat*>(animal);
+        Cat* c_ptr = dynamic_cast<Cat*>(animal);
+        if (c_ptr != nullptr)
+            std::cout << "cat spy data: " << c_ptr->spy_data << '\n';
+        else
+            std::cout << "Dis is not a cat\n";
+        // type1 x;
+        // type2 y = (type2)x;
+    }
 
     // Animal a = Cat();
     // a.roar();
